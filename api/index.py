fc2eb2a97c520187cbd187d0437f5ba48fff59f6
shivamkr23 <shivamkr.kr23@gmail.com>
2025-01-11 18:15:44 +0530

    fix(api): added post method on api

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 # /api/index.py
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Flask Vercel Example - Hello World", 200
+
+@app.route("/api", methods=['GET', 'POST'])
+def api():
+    if request.method == 'POST':
+        return jsonify({"marks": 100}), 200
+    else:
+        return jsonify({"marks": 0}), 200
+
 
 
 @app.errorhandler(404)
