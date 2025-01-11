609f584de8648480547cc1aba75842326bd0257a
shivamkr23 <shivamkr.kr23@gmail.com>
2025-01-11 18:05:11 +0530

    fix(api): add 404 not found

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask
+# /api/index.py
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "Flask Vercel Example - Hello World", 200
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"status": 404, "message": "Not Found"}), 404
+
