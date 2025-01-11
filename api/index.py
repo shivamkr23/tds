dca27e8fd8ae43d0cae98bb4b0cfd67728d11b30
shivamkr23 <shivamkr.kr23@gmail.com>
2025-01-11 19:22:08 +0530

    fix(api): fixed get method on api

--- api/index.py
+++ api/index.py
@@ -8,11 +8,6 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Flask Vercel Example - Hello World", 200
-
-
 @app.route("/api")
 def api():
     query = request.args.getlist("name")
@@ -21,13 +16,7 @@ def api():
 
     ans = []
     for d in data:
-        if d['name'] in query:
-            ans.append(d['marks'])
+        if d["name"] in query:
+            ans.append(d["marks"])
 
     return jsonify({"marks": ans}), 200
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return jsonify({"status": 404, "message": "Not Found"}), 404
-
