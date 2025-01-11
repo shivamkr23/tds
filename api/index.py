dbe49e72f0cb38aba281e348d37b60e740d073b1
shivamkr23 <shivamkr.kr23@gmail.com>
2025-01-11 19:03:35 +0530

    fix(api): fix the api for name and multiple name queries

--- api/index.py
+++ api/index.py
@@ -1,5 +1,8 @@
 # /api/index.py
 
+import json
+from os.path import join
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -9,13 +12,19 @@ app = Flask(__name__)
 def home():
     return "Flask Vercel Example - Hello World", 200
 
-@app.route("/api", methods=['GET', 'POST'])
-def api():
-    if request.method == 'POST':
-        return jsonify({"marks": 100}), 200
-    else:
-        return jsonify({"marks": 0}), 200
 
+@app.route("/api")
+def api():
+    names = request.args.getlist("name")
+    with open(join("data", "marks.json"), "r") as file:
+        marks = json.load(file)
+
+    ans = []
+    for name in names:
+        if name in marks:
+            ans.append(marks[name])
+
+    return jsonify({"marks": ans}), 200
 
 
 @app.errorhandler(404)
