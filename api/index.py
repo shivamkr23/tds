f7e280c4489b10a05bc9cb300fd4ee36499f8f50
shivamkr23 <shivamkr.kr23@gmail.com>
2025-01-12 12:05:31 +0530

    fix(html): make the api working again

--- api/index.py
+++ api/index.py
@@ -1,18 +1,25 @@
 import json
+from os.path import join
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
+with open(join("data", "marks.json"), "r") as file:
+    datas = json.load(file)
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_url = urlparse(self.path)
         query = parse_qs(parsed_url.query)
+        names = query["name"]
 
-        names = query.get("names")
+        ans = []
+        for data in datas:
+            if data["name"] in names:
+                ans.append(data["marks"])
 
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        self.wfile.write(json.dumps({"marks": names}).encode("utf-8"))
+        self.wfile.write(json.dumps({"marks": ans}).encode("utf-8"))
         return
